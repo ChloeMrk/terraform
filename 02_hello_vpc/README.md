e0e828fb8b2c2d519c83735b9254850ef68fdad8
ChloeMrk <56391670+ChloeMrk@users.noreply.github.com>
2023-11-23 12:41:44 +0100

    exo2

--- 02_hello_vpc/README.md
+++ 02_hello_vpc/README.md
@@ -1,2 +1,6 @@
 # Source
 https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc.html
+https://registry.terraform.io/providers/hashicorp/aws/2.69.0/docs/resources/subnet
+# Problème rencontrer
+
+Récupérer ID du VPC
--- 02_hello_vpc/main.tf
+++ 02_hello_vpc/main.tf
@@ -8,8 +8,8 @@ resource "aws_vpc" "vpc-test" {
     cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "subnet-1" {
-    vpc_id = "aws_vpc.vpc-test.id"
+resource "aws_subnet" "subnet-test-1" {
+    vpc_id = aws_vpc.vpc-test.id
     cidr_block = "10.0.1.0/24"
-    
+
 }
